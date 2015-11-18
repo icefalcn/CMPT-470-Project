df779f971f6062f1aa1ec687b0a9f3cf954afdec
yec1 <yec1@sfu.ca>
2015-11-17 16:42:34 -0800

    Fixed commit error

--- blog/app/models/contact.rb
+++
@@ -1,4 +0,0 @@
-class Contact < ActiveRecord::Base
-  validates :First_Name, presence: true
-  validates :Last_Name, presence: true
-end
--- chef/cookbooks/baseconfig/recipes/default.rb
+++ chef/cookbooks/baseconfig/recipes/default.rb
@@ -1,7 +1,4 @@
-# Make sure the Apt package lists are up to date, so we're downloading versions that exist.
-execute 'key_update' do
-  command 'apt-key update'
-end 
+
 
 execute 'apt_update' do
   command 'apt-get update'
@@ -11,10 +8,19 @@ end
 package "apache2"
 package "libapache2-mod-passenger"
 
+package "ruby1.9.1-dev"
+package "ruby-dev"
+package "zlib1g-dev"
+package "postgresql"
 package "wget"
 package "ntp"
-package "postgresql"
+package "sqlite3"
 package "ack-grep"
+package "libsqlite3-dev"
+package "nodejs"
+package "libpq-dev"
+package "ri1.9.1"
+package 'rbenv'
 
 cookbook_file "ntp.conf" do
   path "/etc/ntp.conf"
@@ -23,48 +29,31 @@ execute 'ntp_restart' do
   command 'service ntp restart'
 end
 
-# rails setup
-package 'ruby1.9.1-dev'
-package 'ruby1.9.1'
-package 'ri1.9.1'
-package 'rbenv'
-package 'rdoc1.9.1'
-package 'irb1.9'
-package 'libreadline-ruby1.9.1'
-package 'libruby1.9.1'
-package 'libxslt-dev'
-package 'libxml2-dev'
-package 'zlib1g-dev'
-package 'libsqlite3-dev'
-package 'nodejs'
-package 'make'
-package 'libpq-dev'
-
-
-execute 'install rails gem' do
-    command 'gem install rails --no-ri --no-rdoc'
+execute 'install_rails' do 
+  command 'gem install --no-rdoc --no-ri rails'
 end
 
 execute 'install passenger' do
   command 'gem install passenger'
 end
 
+execute 'postgres_user' do
+  command 'echo "CREATE DATABASE mydb; CREATE USER vagrant; GRANT ALL PRIVILEGES ON DATABASE mydb TO vagrant;" | sudo -u postgres psql'
+end
+
+execute 'create_db' do
+  command "echo 'CREATE TABLE contacts( fname varchar NOT NULL, lname varchar NOT NULL, email varchar NOT NULL, pnum varchar NOT NULL, notes text NOT NULL, created_at datetime NOT NULL, updated_at datetime NOT NULL);' | sudo -u vagrant psql mydb"
+end
 
 execute 'bundle_inst' do
-    cwd '/home/vagrant/project/blog'
+    cwd '/home/vagrant/project/webroot'
     command 'bundle install'
     user 'vagrant'
 end
 
-#execute 'run server' do
-#    cwd '/home/vagrant/project/blog'
-#    command './bin/rails server -d -b 0.0.0.0'
-#    user 'vagrant'
-#end
-
 execute 'migrate' do
-  cwd '/home/vagrant/project/blog/bin'
-  command 'rake db:migrate -V'
+  cwd '/home/vagrant/project/webroot/bin'
+  command 'rake db:migrate RAILS_ENV=production'
   user 'vagrant'
 end
 
@@ -74,8 +63,4 @@ end
 
 execute 'apache2_restart' do
   command 'service apache2 restart'
-end
-
-execute 'postgres_user' do
-  command 'echo "CREATE DATABASE mydb; CREATE USER vagrant; GRANT ALL PRIVILEGES ON DATABASE mydb TO vagrant;" | sudo -u postgres psql'
 end
