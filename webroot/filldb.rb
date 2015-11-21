da66dbdb6135cb745e3212524af32cc1e7a23745
yec1 <yec1@sfu.ca>
2015-11-20 18:22:48 -0800

    Trying to get chef to run ruby script

---
+++ webroot/filldb.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'json'
+require 'pg'
+upcoming = File.read('upcoming.json')
+upcoming_hash = JSON.parse(upcoming)
+current = File.read('current.json')
+current_hash = JSON.parse(current)
+
+conn = PG.connect(:dbname => 'mydb', :user => 'vagrant')
+conn.prepare('insert_movie', 'insert into movie(title, author, genre, year, rating, urlink, synopsys) values ($1, $2, $3, $4, $5, $6, $7)')
+upcoming_hash["results"].each do |movie|
+	title =movie["title"]
+	if movie["release_date"].nil?
+		release = ""
+	else
+		release = movie["release_date"]
+	end
+	if movie["poster_path"].nil?
+		poster = ""
+	else
+		poster = 'http://image.tmdb.org/t/p/w500' + movie["poster_path"]
+	end
+	if movie["overview"].nil?
+		overview = ""
+	else
+		overview = movie["overview"]
+	end
+
+	conn.exec_prepared('insert_movie', [title, "", "", release, 0, poster, overview])
+end
+
+current_hash["results"].each do |movie|
+	title =movie["title"]
+	if movie["release_date"].nil?
+		release = ""
+	else
+		release = movie["release_date"]
+	end
+	if movie["poster_path"].nil?
+		poster = ""
+	else
+		poster = 'http://image.tmdb.org/t/p/w500' + movie["poster_path"]
+	end
+	if movie["overview"].nil?
+		overview = ""
+	else
+		overview = movie["overview"]
+	end
+	
+
+	conn.exec_prepared('insert_movie', [title, "", "", release, 0, poster, overview])
+end
