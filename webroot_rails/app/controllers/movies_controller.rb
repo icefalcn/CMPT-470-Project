374a27eea2567f72665abb479628c359495623ad
cla137 <cla137@sfu.ca>
2015-11-26 21:51:02 -0800

    add voting logic

--- webroot_rails/app/controllers/movies_controller.rb
+++ webroot_rails/app/controllers/movies_controller.rb
@@ -7,14 +7,17 @@ class MoviesController < ApplicationController
   def search
   	if params[:search]
       @movies = Movie.search(params[:search]).order("title DESC")
-    # else
-    #   @movies = Movie.all
+    else
+      @movies = Movie.all
     end
    end
  
   def show
     @movie = Movie.find(params[:id])
     @actors = @movie.actors
+
+    
+
   end
   
   def create 
@@ -32,18 +35,40 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
+    @movie.update_attribute(:title, "there")
+    # respond_with @movie
       if @movie.update(movie_params)
+        @movie.update_attribute(:title, "if")
         redirect_to @movie
       else
+        @movie.update_attribute(:title, "else")
         render 'edit'
       end
   end
 
+  def upvote
+    @movie = Movie.find(params[:id])
+    # @movie.increment!(:rating,1)
+    @movie.update_attribute(:title, "up")
+    # redirect_to '/index' 
+    # render '/index'
+  end
+
+  def downvote
+    @movie = Movie.find(params[:id])
+    # @movie.increment!(:rating,-1)
+    @movie.update_attribute(:title, "down")
+  end
+
+  # helper_method :downvote
+  # helper_method :upvote
+  # helper :all
 
   private 
   def movie_params 
     params.require(:movie).permit(:title,:image,:release_year,:plot) 
   end
+
 
 
 
