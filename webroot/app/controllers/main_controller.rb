2c5dfec6c711510c825d63d4ea88d7d47a8798af
assaini <assaini@sfu.ca>
2015-11-29 17:42:56 -0800

    Main Page done and fixed CSS

--- webroot/app/controllers/main_controller.rb
+++ webroot/app/controllers/main_controller.rb
@@ -1,5 +1,8 @@
 class MainController < ApplicationController
   def index
-    @movies = Movies.last(5)
+    @action = Movies.where("genre like ?", "%Action%").order("rating").first(3)
+    @comedy = Movies.where("genre like ?", "%Comedy%").order("rating").first(3)
+    @animation = Movies.where("genre like ?", "%Animation%").order("rating").first(3)
+    @romance = Movies.where("genre like ?", "%Romance%").order("rating").first(3)
   end
 end
