7bcdcdb563907cd4b425ae08ade7eff0e7241428
icefalcn <yla263@sfu.ca>
2015-12-05 22:03:42 -0800

    remove from wachlist button

--- webroot/app/controllers/watchlist_controller.rb
+++ webroot/app/controllers/watchlist_controller.rb
@@ -2,4 +2,7 @@ class WatchlistController < ApplicationController
   def index
     @watchlist = Watchlist.select("*")
   end
+  def remove
+  	@remove = Watchlist.delete(params[:movieid])
+  end
 end
